9ed19ffdb44093954e2679da8058356f4ffbe981
Saurabh Wagh <saurabhwagh.sw.12@gmail.com>
2024-01-03 11:16:53 +0530

    Updated CountDown

--- Components/Countdown.js
+++ Components/Countdown.js
@@ -1,9 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 
-// import { fontSizes, spacing } from '../utils/sizes';
-// import { colors } from '../utils/colors';
-
 const minutesToMillis = (min) => min * 1000 * 60;
 const formatTime = (time) => (time < 10 ? `0${time}` : time);
 
